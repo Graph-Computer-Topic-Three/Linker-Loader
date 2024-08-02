8900986710fd11c9d643b602cafbd38133fb9890
gaohongy <hongyu.gao2001@gmail.com>
2024-08-02 22:38:30 +0800

    完成 section 提取 20240802223821      1		modified:   fle.cpp      2		modified:   ld      3		new file:   test.sh

--- fle-cpp/fle.cpp
+++ fle-cpp/fle.cpp
@@ -29,6 +29,8 @@
 #include <functional>
 #include <cstring>
 #include <fstream>
+#include <utility>
+#include <sstream>
 
 #include "json.hpp"
 
@@ -51,6 +53,43 @@ std::string getLastPathComponent(const std::string& path) {
     return path;
 }
 
+class Section {
+
+    public:
+
+    Section() : name(""), data() {}
+
+    Section(std::string name, const std::vector<std::pair<std::string, std::string>> &data) : name(name), data(data) {}
+
+    std::string getName() {
+        return name;
+    }
+
+    std::vector<std::pair<std::string, std::string>> getData() {
+        return data;
+    }
+    void setName(std::string name) {
+        this->name = name;
+    }
+    
+    void setData(const std::vector<std::pair<std::string, std::string>> &data) {
+        this->data = data;
+    }
+
+    std::string toString() {
+        std::stringstream ss;
+        ss << "Section name: " << name << std::endl;
+        ss << "Section data: " << std::endl;
+        for (auto &pair : data) {
+            ss << "\t" << pair.first << ": " << pair.second << std::endl;
+        }
+        return ss.str();
+    }
+
+    private:
+    std::string name;
+    std::vector<std::pair<std::string, std::string>> data;
+};
 
 int main(int argc, char *argv[]) {
     // Initialize the functions name and functional pointer mapping
@@ -90,16 +129,29 @@ int main(int argc, char *argv[]) {
 void FLE_ld(int argc, char *argv[]) {
 
     std::string dest;
-    std::unordered_map<
-                        std::string, 
-                        std::vector<std::pair<std::string, std::string>>
-    > sections;
+    std::unordered_map<std::string, std::vector<Section>> sections;
+
+    //std::unordered_map<
+    //                    std::string, 
+    //                    std::vector<
+    //                        std::pair<std::string, std::vector<std::pair<std::string, std::string>>>
+    //                    >
+    //> sections;
+
+    #ifdef DEBUG
+    std::cout << "==================== argc & argv ====================" << std::endl;
+    std::cout << argc << std::endl;
+    for (int i = 0; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
+    std::cout << "==================== end argc & argv ====================" << std::endl;
+    #endif
 
     // 提取所有 section
     for (int i = 0; i < argc; i++) {
         if (!strcmp(argv[i], "-o")) continue;
 
-        if (i > 0 && strcmp(argv[i - 1], "-o")) {
+        if (i > 0 && !strcmp(argv[i - 1], "-o")) {
             // 提取链接输出文件名
             dest = std::string(argv[i]);
         } else {
@@ -109,34 +161,58 @@ void FLE_ld(int argc, char *argv[]) {
             assert(j["type"] == ".obj");
 
             // 存储所有 section
+            //for (json::iterator it = j.begin(); it != j.end(); ++it) {
+            //    if (it.key()[0] == '.') { // is a section
+            //        std::vector<std::string> section_data = it.value();
+            //        std::vector<std::pair<std::string, std::string>> section_data_vec;
+
+            //        for (std::string &data : section_data) {
+            //            auto item = std::make_pair(data.substr(0, data.find(':')), data.substr(data.find(':') + 1));
+            //            section_data_vec.push_back(item);
+            //        }
+
+            //        sections[argv[i]].push_back(std::make_pair(it.key(), section_data_vec));
+            //    }
+            //}
+
             for (json::iterator it = j.begin(); it != j.end(); ++it) {
                 if (it.key()[0] == '.') { // is a section
                     std::vector<std::string> section_data = it.value();
+                    std::vector<std::pair<std::string, std::string>> section_data_vec;
+
                     for (std::string &data : section_data) {
                         auto item = std::make_pair(data.substr(0, data.find(':')), data.substr(data.find(':') + 1));
-                        sections[it.key()].push_back(item);
+                        section_data_vec.push_back(item);
                     }
+
+                    sections[argv[i]].push_back(Section(it.key(), section_data_vec));
                 }
             }
+
+            f.close();
         }
     } 
 
     #ifdef DEBUG
+    #ifdef extrace-section-test
     std::cout << "===== sections =====" << std::endl;
 
-    for (auto &section : sections) {
-        std::cout << section.first << std::endl;
+    std::cout << sections.size() << std::endl;
 
-        auto &item = section.second;
-        for (auto &it : item) {
-            std::cout << it.first << " " << it.second << std::endl;
+    for (auto &sections_single_file : sections) { // 枚举每一个文件下的 section
+        std::cout << sections_single_file.first << std::endl;
+
+        auto &section_vec = sections_single_file.second;
+        for (auto &section : section_vec) {
+            std::cout << section.toString() << std::endl;
         }
 
-        std::cout << "=====" << std::endl;
+        std::cout << "-------------------" << std::endl << std::endl;
     }
 
     std::cout << "===== sections end =====" << std::endl;
-    #endif
+    #endif // extrace-section-test
+    #endif // DEBUG 
 
 
 }
---
+++ fle-cpp/test.sh
@@ -0,0 +1,4 @@
+#g++ fle.cpp -std=c++11 -DDEBUG -o ld
+g++ fle.cpp -std=c++11 -o ld
+
+./ld json-test/main.fle json-test/foo.fle
